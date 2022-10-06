398c9a84a894e254b216599a1b271e9e65a05749
Wijtze <26749380+Wijtze@users.noreply.github.com>
2022-10-06 22:30:56 +0200

    Update project structure

---
+++ cmd/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+// setupLogger checks whether the Stdout is a terminal or not.
+// if so it sets the global log's writer to a ConsoleWriter.
+func setupLogger() {
+	if fileInfo, _ := os.Stdout.Stat(); (fileInfo.Mode() & os.ModeCharDevice) != 0 {
+		logOutput := zerolog.ConsoleWriter{Out: os.Stderr}
+		log.Logger = log.Output(logOutput)
+
+		return
+	}
+
+	log.Logger = log.Output(os.Stderr)
+}
+
+// setLogLevel sets the global logging level of the log's writer.
+func setLogLevel(level zerolog.Level) {
+	zerolog.SetGlobalLevel(level)
+}
+
+func main() {
+	setupLogger()
+	setLogLevel(zerolog.DebugLevel)
+
+	log.Info().Msgf("%s", greet("John"))
+}
+
+func greet(s string) string {
+	return fmt.Sprintf("Hello %s!", s)
+}
