f49634a7aa0d72ab2c6523549e363be9bf0481b8
Mr Hawk <heindouwe.havik@crisis-training.nl>
2022-12-05 23:05:01 +0100

    Added random string generator

--- blockchain/wallet.go
+++ blockchain/wallet.go
@@ -1,3 +1,32 @@
 package blockchain
 
-type Wallet struct{}
+import (
+	"math/rand"
+)
+
+type Wallet struct {
+	privateKey string
+	publicKey  string
+}
+
+const keyBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+func randomStringBytes(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = keyBytes[rand.Intn(len(keyBytes))]
+	}
+	return string(b)
+}
+
+// func genPubKey() string {
+
+// }
+
+// func genPrivKey() string {
+
+// }
+
+// func createWallet(privKey string, publicKey string) Wallet {
+// 	return Wallet()
+// }
