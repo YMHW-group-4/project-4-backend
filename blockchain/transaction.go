57cf60e1997d72127cb067f10658973704a2613e
Wijtze <26749380+Wijtze@users.noreply.github.com>
2023-01-18 00:15:21 +0100

    Fixed typo

--- blockchain/transaction.go
+++ blockchain/transaction.go
@@ -1,12 +1,13 @@
 package blockchain
 
+// Transaction represents a transaction within the blockchain.
 type Transaction struct {
-	Sender    string
-	Receiver  string
-	Signature string
-	Amount    float32
-	Nonce     uint32
-	Timestamp int64
+	Sender    string  `json:"sender"`
+	Receiver  string  `json:"receiver"`
+	Signature string  `json:"signature"`
+	Amount    float32 `json:"amount"`
+	Nonce     uint32  `json:"nonce"`
+	Timestamp int64   `json:"timestamp"`
 }
 
 //func CreateTransaction(amount float32) Transaction {
